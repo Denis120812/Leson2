274425a362048c802f22ce53d92d212794fc29d3
Denis Shestakov <azia1864@gmail.com>
2024-01-19 08:05:15 +0100

    add Les2:Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+1. Создать массив на 5 элементов
+2. Заполнить его целыми числами
+3. Вывести эдементы на экран
+*/
+
+int[] array = new int[5];
+array[0] = 5;
+array[1] = 7;
+array[2] = 5;
+array[3] = 4;
+array[4] = 3;
+
+System.Console.WriteLine(array[0]);
+System.Console.WriteLine(array[1]);
+System.Console.WriteLine(array[2]);
+System.Console.WriteLine(array[3]);
+System.Console.WriteLine(array[4]);
+System.Console.WriteLine(array[5]);
+
+// Присвоение значений при создании массива
+
+int[] array2 = new int[5] { 5, 7, 5, 4, 3 };
+
+// Если массив не большой - упрощенный способ
+
+int[] array3 = { 5, 7, 5, 4, 3 };
